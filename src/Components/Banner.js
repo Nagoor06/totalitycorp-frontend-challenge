3789506ee8a5d6159c1d19b4a2333b8f4031e57c
Nagoor06 <113981524+Nagoor06@users.noreply.github.com>
2023-10-06 22:42:01 +0530

    Initial commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,11 @@
+import "../Styles/Banner.css";
+
+export default function Banner() {
+  return (
+    <div className="Banner-container">
+      <div className="Banner">
+       
+      </div>
+    </div>
+  );
+}
